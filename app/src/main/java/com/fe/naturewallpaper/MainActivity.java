ec16301e8157ba807f84cddedd0042f95b722f63
Monyneth <monynethsok33@gmail.com>
2018-11-29 23:14:44 +0700

    First commit

--- app/src/main/java/com/fe/naturewallpaper/MainActivity.java
+++ app/src/main/java/com/fe/naturewallpaper/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    //first commit
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
