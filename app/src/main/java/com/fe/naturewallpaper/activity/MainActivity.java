1ada5b281c05279fa97309570cebf1f376c52598
khantey1998 <khanteylong@gmail.com>
2019-02-10 18:45:18 +0700

    save wallpaper to database

--- app/src/main/java/com/fe/naturewallpaper/activity/MainActivity.java
+++ app/src/main/java/com/fe/naturewallpaper/activity/MainActivity.java
@@ -1,10 +1,14 @@
 package com.fe.naturewallpaper.activity;
 
 import android.app.FragmentManager;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -14,12 +18,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.fe.naturewallpaper.R;
+import com.fe.naturewallpaper.fragment.GalleryFragment;
+import com.fe.naturewallpaper.fragment.SettingFragment;
 import com.fe.naturewallpaper.fragment.ShareWallpaperFragment;
+import com.google.firebase.auth.FirebaseAuth;
+import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+    private ImageView imgProfile;
+    private TextView txtHeader;
+    private TextView  txtSubHeader;
+    private FirebaseAuth auth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +52,19 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
+        View header=navigationView.getHeaderView(0);
+        imgProfile = header.findViewById(R.id.image_profile);
+        txtHeader = header.findViewById(R.id.header_title);
+        txtSubHeader = header.findViewById(R.id.header_subtitle);
+
+        auth = FirebaseAuth.getInstance();
+        String  email = auth.getCurrentUser().getEmail();
+        String username = auth.getCurrentUser().getDisplayName();
+        String profileUri = auth.getCurrentUser().getPhotoUrl().toString();
+        txtHeader.setText(username);
+        txtSubHeader.setText(email);
+        Picasso.get().load(profileUri).into(imgProfile);
+        onGalleryClick();
     }
 
     @Override
@@ -78,11 +106,11 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_upload) {
-            onUploadButttonClick();
+            chooseImageFromGallery();
         } else if (id == R.id.nav_gallery) {
-
+            onGalleryClick();
         } else if (id == R.id.nav_manage) {
-
+            onSettingClick();
         } else if (id == R.id.nav_share) {
 
         } else if (id == R.id.nav_send) {
@@ -93,10 +121,35 @@ public class MainActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+    private void chooseImageFromGallery() {
+        Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        galleryIntent.setType("image/*");
+        startActivityForResult(galleryIntent, 1);
+    }
 
-    private void onUploadButttonClick(){
-        ShareWallpaperFragment wallpaperFragment = new ShareWallpaperFragment();
-        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction().replace(R.id.layout_content, wallpaperFragment);
+    private void onGalleryClick(){
+        GalleryFragment galleryFragment = new GalleryFragment();
+        FragmentTransaction fragmentTransaction =  getSupportFragmentManager().beginTransaction().replace(R.id.layout_content, galleryFragment);
         fragmentTransaction.commit();
+    }
+
+    private void onSettingClick(){
+        SettingFragment settingFragment = new SettingFragment();
+        FragmentTransaction fragmentTransaction =  getSupportFragmentManager().beginTransaction().replace(R.id.layout_content, settingFragment);
+        fragmentTransaction.commit();
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK && data != null && data.getData() != null){
+            Bundle imgBundle = new Bundle();
+            Uri imgUri = data.getData();
+            imgBundle.putString("imagePath", imgUri.toString());
+            ShareWallpaperFragment wallpaperFragment = new ShareWallpaperFragment();
+            wallpaperFragment.setArguments(imgBundle);
+            FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.layout_content, wallpaperFragment);
+            fragmentTransaction.commit();
+        }
     }
 }
--- app/src/main/java/com/fe/naturewallpaper/fragment/ShareWallpaperFragment.java
+++ app/src/main/java/com/fe/naturewallpaper/fragment/ShareWallpaperFragment.java
@@ -1,14 +1,30 @@
 package com.fe.naturewallpaper.fragment;
-
-
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.fe.naturewallpaper.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -18,13 +34,78 @@ public class ShareWallpaperFragment extends Fragment {
     public ShareWallpaperFragment() {
         // Required empty public constructor
     }
+    private EditText etxtTitle;
+    private EditText etxtContent;
+    private EditText etxtTag;
+    private Button uploadButton;
+    private ImageView img;
+    private Uri imgUri;
+    private FirebaseFirestore firestore;
+    private StorageReference storage;
+    FirebaseAuth auth;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_share_wallpaper, container, false);
+
+        etxtContent = view.findViewById(R.id.img_content);
+        etxtTag = view.findViewById(R.id.img_tag);
+        etxtTitle = view.findViewById(R.id.img_title);
+        uploadButton =view.findViewById(R.id.upload_button);
+        img = view.findViewById(R.id.uploaded_img);
+
+        firestore = FirebaseFirestore.getInstance();
+        storage = FirebaseStorage.getInstance().getReference();
+        auth = FirebaseAuth.getInstance();
+
+
+        Bundle bundle = this.getArguments();
+        String path = bundle.getString("imagePath");
+        imgUri = Uri.parse(path);
+        img.setImageURI(imgUri);
+
+        uploadButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveImageToFirebase();
+            }
+        });
         return view;
     }
 
+    public void saveImageToFirebase(){
+        final StorageReference storageReference = storage.child(etxtTitle.getText().toString()+auth.getCurrentUser().getUid()+".jpg");
+        storageReference.putFile(imgUri).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task) {
+                if (task.isSuccessful()){
+                    final Map<String, Object> wallpaper = new HashMap<>();
+                    storageReference.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+                        @Override
+                        public void onSuccess(Uri uri) {
+                            wallpaper.put("url", uri.toString());
+                            wallpaper.put("title", etxtTitle.getText().toString());
+                            wallpaper.put("content",etxtContent.getText().toString());
+                            wallpaper.put("tag", etxtTag.getText().toString());
+                            firestore.collection("image_url").document().set(wallpaper).addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    GalleryFragment galleryFragment = new GalleryFragment();
+                                    FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
+                                    fragmentTransaction.replace(R.id.layout_content,galleryFragment);
+                                    fragmentTransaction.commit();
+                                }
+                            });
+                        }
+                    });
+                }
+                else{
+                    Toast.makeText(getContext(),"Error Sign up:"+task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                    Log.d("Tag", "Error: "+task.getException().toString());
+                }
+            }
+        });
+    }
 }
